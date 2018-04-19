0b97006367d05a82da81e7386602027aeb34c2ac
dastyk <dastykill@gmail.com>
2018-04-19 18:36:21 +0200

    Error message, pretty nice. Removed OBJParser

---
+++ Utilities/Include/Error.h
@@ -0,0 +1,58 @@
+#ifndef _UTILITIES_ERROR_H_
+#define _UTILITIES_ERROR_H_
+#include "CompileTimeString.h"
+#include <string>
+
+#define PASS_IF_ERROR(e) if(e.hash != "Success"_hash) return e
+
+#define RETURN_SUCCESS return (Utilities::__lastError__ = Utilities::CreateError("Success", __FILE__, __LINE__))
+#define RETURN_ERROR(msg) return (Utilities::__lastError__ = Utilities::CreateError(msg, __FILE__, __LINE__))
+#define RETURN_ERROR_EX(msg, extra) return Utilities::CreateErrorExtra(Utilities::CreateError(msg, __FILE__, __LINE__), extra)
+#define ERROR const Utilities::Error&
+
+
+namespace Utilities
+{
+	
+	struct Error
+	{
+		const char* message;
+		Utilities::HashValue hash;
+		const char* file;
+		uint32_t line;
+	};
+	static Error __lastError__;
+	static std::string __lastErrorString__;
+	constexpr Error CreateError(const StringHash::ConstexprString message, const char* file, uint32_t line)
+	{
+		return { message, StringHash::GetHash_ConstexprString(message), file, line };
+	}
+	template<typename extraType>
+	ERROR CreateErrorExtra(const Error& err, extraType extra)
+	{
+		__lastError__.message = (__lastErrorString__ = err.message + std::to_string(extra)).c_str();
+		__lastError__.hash = StringHash::GetHash_ConstexprString(__lastErrorString__.c_str(), uint32_t(__lastErrorString__.size()));
+		__lastError__.file = err.file;
+		__lastError__.line = err.line;
+		return __lastError__;
+	}
+	template<>
+	ERROR CreateErrorExtra(const Error& err, std::string extra)
+	{
+		__lastError__.message = (__lastErrorString__ = err.message + extra).c_str();
+		__lastError__.hash = StringHash::GetHash_ConstexprString(__lastErrorString__.c_str(), uint32_t(__lastErrorString__.size()));
+		__lastError__.file = err.file;
+		__lastError__.line = err.line;
+		return __lastError__;
+	}
+	template<>
+	ERROR CreateErrorExtra(const Error& err, const char* extra)
+	{
+		__lastError__.message = (__lastErrorString__ = err.message + std::string(extra)).c_str();
+		__lastError__.hash = StringHash::GetHash_ConstexprString(__lastErrorString__.c_str(), uint32_t(__lastErrorString__.size()));
+		__lastError__.file = err.file;
+		__lastError__.line = err.line;
+		return __lastError__;
+	}
+}
+#endif
---
+++ Utilities/UtilitiesTests/ErrorTest.cpp
@@ -0,0 +1,48 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include"../Include/Error.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UtilitiesTests
+{
+	TEST_CLASS(ERRORTest)
+	{
+	public:
+		ERROR Function(int i)
+		{
+			if (i == 0)
+				RETURN_ERROR("i == 0");
+
+			RETURN_SUCCESS;
+		}
+		ERROR Foo()
+		{
+			PASS_IF_ERROR(Function(0));
+			RETURN_SUCCESS;
+		}
+		ERROR Foo_int(int i)
+		{
+			RETURN_ERROR_EX("Error ", i);
+		}
+		ERROR Foo_char()
+		{
+			RETURN_ERROR_EX("Error ", "char");
+		}
+		ERROR Foo_str()
+		{
+			RETURN_ERROR_EX("Error ", std::string("char"));
+		}
+		TEST_METHOD(ERRORTest_Basic)
+		{
+			Assert::AreEqual("Success"_hash, Function(1).hash);
+			Assert::AreEqual("i == 0"_hash, Foo().hash);
+		}
+		TEST_METHOD(ERRORTest_Extra)
+		{
+			Assert::AreEqual("Error 1"_hash, Foo_int(1).hash);
+			Assert::AreEqual("Error 2"_hash, Foo_int(2).hash);
+			Assert::AreEqual("Error char"_hash, Foo_char().hash);
+			Assert::AreEqual("Error char"_hash, Foo_str().hash);
+		}
+	};
+}
