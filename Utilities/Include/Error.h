ae84aabe9d61ba75cbea0361f1ca4703b8c8fe82
dastyk <dastykill@gmail.com>
2018-04-19 19:24:33 +0200

    Throw if hr error

--- Utilities/Include/Error.h
+++ Utilities/Include/Error.h
@@ -11,6 +11,7 @@
 #define RETURN_IF_HR_ERROR(hr, msg) if(FAILED(hr)) RETURN_ERROR_EX(msg, hr)
 #define THROW_ERROR(msg) throw (Utilities::__lastError__ = Utilities::CreateError(msg, __FILE__, __LINE__))
 #define THROW_ERROR_EX(msg, extra) throw Utilities::CreateErrorExtra(Utilities::CreateError(msg, __FILE__, __LINE__), extra)
+#define THROW_IF_HR_ERROR(hr, msg) if(FAILED(hr)) THROW_ERROR_EX(msg, hr)
 #define UERROR const Utilities::Error&
 
 
