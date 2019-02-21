c6674d9b312fdb7f54d9f182ed7b6035fb4dacb4
dastyk <dastykill@gmail.com>
2019-02-21 20:06:07 +0100

    Base what

--- Utilities/Include/ErrorHandling.h
+++ Utilities/Include/ErrorHandling.h
@@ -1,11 +1,18 @@
 #ifndef _UTILITIES_ERROR_HANDLING_H_
 #define _UTILITIES_E#RROR_HANDLING_H_
+#include <exception>
 #include <string>
 
 namespace Utilities
 {
-	struct Exception {
-		virtual const std::string what() const = 0;
+	struct Exception : public std::exception {
+		Exception(std::string&& what) : _what(what) { }
+		virtual const char* what() const
+		{
+			return _what.c_str();
+		}
+	private:
+		std::string _what;
 	};
 }
 #endif
