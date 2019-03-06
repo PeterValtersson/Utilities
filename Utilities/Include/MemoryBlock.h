221014a11dba3f31c4f7e5236fb2c107a2e59da4
dastyk <dastykill@gmail.com>
2019-03-06 19:19:21 +0100

    Moved struct to MemoryBlock.h

--- Utilities/Include/MemoryBlock.h
+++ Utilities/Include/MemoryBlock.h
@@ -8,6 +8,30 @@ namespace Utilities
 			void* data;
 			size_t size;
 		};
+
+
+		using Handle = size_t;
+		struct ChunkyData {
+			ChunkyData( ChunkyAllocator& chunkyAllocator, Handle handle, MemoryBlock data ) : chunkyAllocator( chunkyAllocator ), handle( handle ), _data( data )
+			{
+				chunkyAllocator.tallyUp( handle );
+			}
+			~ChunkyData()
+			{
+				chunkyAllocator.tallyDown( handle );
+			}
+			ChunkyData( const ChunkyData& other ) : chunkyAllocator( other.chunkyAllocator ), handle( other.handle ), _data( other._data )
+			{
+				chunkyAllocator.tallyUp( handle );
+			}
+			ChunkyData& operator=( const ChunkyData& other ) = delete;
+
+			inline MemoryBlock& data() { return _data; }
+		private:
+			ChunkyAllocator & chunkyAllocator;
+			Handle handle;
+			MemoryBlock _data;
+		};
 	}
 }
 
