910afc798ef39133f2b229a554d9133b1eff5e78
dastyk <dastykill@gmail.com>
2019-05-14 21:25:23 +0200

    ¨Fixed some "unexcepted token for macros"

--- Utilities/Include/Flags.h
+++ Utilities/Include/Flags.h
@@ -2,13 +2,13 @@
 #ifndef _UTILZ_FLAGS_H_
 #define _UTILZ_FLAGS_H_
 
-#ifndef ENUM_FLAG_OPERATOR(T,X)
+#ifndef ENUM_FLAG_OPERATOR
 #define ENUM_FLAG_OPERATOR(T,X) inline T operator X (const T& lhs, const T& rhs) { return (T) (static_cast<std::underlying_type_t <T>>(lhs) X static_cast<std::underlying_type_t <T>>(rhs)); } 
 #endif
-#ifndef ENUM_FLAG_OPERATOR2(T,X)
+#ifndef ENUM_FLAG_OPERATOR2
 #define ENUM_FLAG_OPERATOR2(T,X) inline void operator X= ( T& lhs,const T& rhs) { lhs = (T)(static_cast<std::underlying_type_t <T>>(lhs) X static_cast<std::underlying_type_t <T>>(rhs)); } 
 #endif
-#ifndef ENUM_FLAGS(T)
+#ifndef ENUM_FLAGS
 #define ENUM_FLAGS(T) \
 inline T operator ~ (const T& t) { return (T) (~static_cast<std::underlying_type_t <T>>(t)); } \
 inline bool operator & (const T& lhs, const T& rhs) { return (static_cast<std::underlying_type_t <T>>(lhs) & static_cast<std::underlying_type_t <T>>(rhs));  } \
