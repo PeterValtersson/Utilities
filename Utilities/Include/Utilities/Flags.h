183de99b00a09c6939e6c250a74333a924aefa3d
Peter <peter.walter.valtersson@gmail.com>
2020-01-03 18:37:28 +0100

    & is now the actual operator, previous funtionallity is now in function flag_has

--- Utilities/Include/Utilities/Flags.h
+++ Utilities/Include/Utilities/Flags.h
@@ -10,11 +10,12 @@
 #endif
 #ifndef ENUM_FLAGS
 #define ENUM_FLAGS(T) \
-inline T operator ~ (const T& t) { return (T) (~static_cast<std::underlying_type_t <T>>(t)); } \
-inline bool operator & (const T& lhs, const T& rhs) { return (static_cast<std::underlying_type_t <T>>(lhs) & static_cast<std::underlying_type_t <T>>(rhs));  } \
+inline T operator~ (const T& t) { return (T) (~static_cast<std::underlying_type_t <T>>(t)); } \
+inline bool flag_has (const T& lhs, const T& rhs) { return (static_cast<std::underlying_type_t <T>>(lhs) & static_cast<std::underlying_type_t <T>>(rhs));  } \
 ENUM_FLAG_OPERATOR2(T,|) \
 ENUM_FLAG_OPERATOR2(T,&) \
 ENUM_FLAG_OPERATOR(T,|) \
+ENUM_FLAG_OPERATOR(T,&) \
 ENUM_FLAG_OPERATOR(T,^)
 #endif
 //enum class T
